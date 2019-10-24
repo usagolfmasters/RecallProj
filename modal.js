f6ba55e46d6f493c7731eee1a524190cbd119a74
usagolfmasters <win@usagolfmasters.com>
2019-10-24 01:59:48 -0700

    Finished Modal 1 with Link

--- modal.js
+++ modal.js
@@ -4,12 +4,10 @@ var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("dist");
-var modalImg = document.getElementById("dist");
+var modalImg = document.getElementById("img1");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
   modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
 }
 
 // Get the <span> element that closes the modal
@@ -18,4 +16,11 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
