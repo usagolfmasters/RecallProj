15cf0afa7a3e60900a160a19bcd0e4abdcc01627
ProbablyRayCruz <probablyraycruz@gmail.com>
2019-10-23 22:59:15 -0700

    Circular Bootstrap
    Attempt to make more responsive using bootstrap
    Circular and image based buttons

---
+++ js/bootstrap-4.0.0.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Recall Gavin Newsom || San Diego Hubs</title>
+    <!-- Bootstrap -->
+    <link href="../bootstrap-4.0.0.css" rel="stylesheet">
+  </head>
+  <body>
+	 
+	 <div class='circle-container'>
+    	<a href='#' class='center'><img src="bootstrap-4.0.0.js" class="rounded-circle img-fluid" alt="Placeholder image"></a>
+    	<a href='#' class='deg0'><img src="bootstrap-4.0.0.js" class="rounded-circle img-fluid" alt="Placeholder image"></a>
+    	<a href='#' class='deg36'><img src="bootstrap-4.0.0.js" class="rounded-circle img-fluid" alt="Placeholder image"></a>
+    	<a href='#' class='deg72'><img src="bootstrap-4.0.0.js" class="rounded-circle img-fluid" alt="Placeholder image"></a>
+    	<a href='#' class='deg108'><img src="bootstrap-4.0.0.js" class="rounded-circle img-fluid" alt="Placeholder image"></a>
+    	<a href='#' class='deg144'><img src="bootstrap-4.0.0.js" class="rounded-circle img-fluid" alt="Placeholder image"></a>
+    	<a href='#' class='deg180'><img src="bootstrap-4.0.0.js" class="rounded-circle img-fluid" alt="Placeholder image"></a>
+		<a href='#' class='deg216'><img src="bootstrap-4.0.0.js" class="rounded-circle img-fluid" alt="Placeholder image"></a>
+    	<a href='#' class='deg252'><img src="bootstrap-4.0.0.js" class="rounded-circle img-fluid" alt="Placeholder image"></a>
+    	<a href='#' class='deg288'><img src="bootstrap-4.0.0.js" class="rounded-circle img-fluid" alt="Placeholder image"></a>
+    	<a href='#' class='deg324'><img src="bootstrap-4.0.0.js" class="rounded-circle img-fluid" alt="Placeholder image"></a>
+    	<a href='#' class='deg360'><img src="bootstrap-4.0.0.js" class="rounded-circle img-fluid" alt="Placeholder image"></a>
+    </div>  
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+    <script src="jquery-3.2.1.min.js"></script> 
+    <!-- Include all compiled plugins (below), or include individual files as needed --> 
+    <script src="popper.min.js"></script> 
+    <script src="bootstrap-4.0.0.js"></script>
+  </body>
+</html>
