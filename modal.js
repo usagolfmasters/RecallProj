a310785bad61bb46619bf69b94d793579ca58ead
usagolfmasters <56849385+usagolfmasters@users.noreply.github.com>
2019-10-26 10:25:51 -0700

    Foundation for Hub Modals
    Prepared CSS and JS files for modals
    Added HTML code for Hubs 1-3

--- modal.js
+++ modal.js
@@ -1,10 +1,326 @@
 // JavaScript Document\
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
+/***********************************
+ * 
+ *Center Modal 
+ * 
+ ***********************************/
+// Get the modal
+var modal = document.getElementById("cModal");
+
+// Get the image and insert it inside the modal
 var img = document.getElementById("dist");
+var modalImg = document.getElementById("img0");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+/***********************************
+ * 
+ *Hub1 Modal 
+ * 
+ ***********************************/
+// Get the modal
+var modal = document.getElementById("h1Modal");
+
+// Get the image and insert it inside the modal
+var img = document.getElementById("hub1");
 var modalImg = document.getElementById("img1");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+/***********************************
+ * 
+ *Hub2 Modal 
+ * 
+ ***********************************/
+// Get the modal
+var modal = document.getElementById("h2Modal");
+
+// Get the image and insert it inside the modal
+var img = document.getElementById("hub2");
+var modalImg = document.getElementById("img2");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+/***********************************
+ * 
+ *Hub3 Modal 
+ * 
+ ***********************************/
+// Get the modal
+var modal = document.getElementById("h3Modal");
+
+// Get the image and insert it inside the modal
+var img = document.getElementById("hub3");
+var modalImg = document.getElementById("img3");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+/***********************************
+ * 
+ *Hub4 Modal 
+ * 
+ ***********************************/
+// Get the modal
+var modal = document.getElementById("h4Modal");
+
+// Get the image and insert it inside the modal
+var img = document.getElementById("hub4");
+var modalImg = document.getElementById("img4");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+/***********************************
+ * 
+ *Hub5 Modal 
+ * 
+ ***********************************/
+// Get the modal
+var modal = document.getElementById("h5Modal");
+
+// Get the image and insert it inside the modal
+var img = document.getElementById("hub5");
+var modalImg = document.getElementById("img5");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+/***********************************
+ * 
+ *Hub6 Modal 
+ * 
+ ***********************************/
+// Get the modal
+var modal = document.getElementById("h6Modal");
+
+// Get the image and insert it inside the modal
+var img = document.getElementById("hub6");
+var modalImg = document.getElementById("img6");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+/***********************************
+ * 
+ *Hub7 Modal 
+ * 
+ ***********************************/
+// Get the modal
+var modal = document.getElementById("h7Modal");
+
+// Get the image and insert it inside the modal
+var img = document.getElementById("hub7");
+var modalImg = document.getElementById("img7");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+/***********************************
+ * 
+ *Hub8 Modal 
+ * 
+ ***********************************/
+// Get the modal
+var modal = document.getElementById("h8Modal");
+
+// Get the image and insert it inside the modal
+var img = document.getElementById("hub8");
+var modalImg = document.getElementById("img8");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+/***********************************
+ * 
+ *Hub9 Modal 
+ * 
+ ***********************************/
+// Get the modal
+var modal = document.getElementById("h9Modal");
+
+// Get the image and insert it inside the modal
+var img = document.getElementById("hub9");
+var modalImg = document.getElementById("img9");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+/***********************************
+ * 
+ *Hub10 Modal 
+ * 
+ ***********************************/
+// Get the modal
+var modal = document.getElementById("h10Modal");
+
+// Get the image and insert it inside the modal
+var img = document.getElementById("hub10");
+var modalImg = document.getElementById("img10");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
   modal.style.display = "block";
