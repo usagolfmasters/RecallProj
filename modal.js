10b985e147aead9c3068169abad3349e70a568aa
usagolfmasters <56849385+usagolfmasters@users.noreply.github.com>
2019-10-26 13:40:56 -0700

    Setup for multi-modal

--- modal.js
+++ modal.js
@@ -3,11 +3,19 @@
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("dist");
-var modalImg = document.getElementById("img1");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
+var img1 = document.getElementById("dist");
+var img2 = document.getElementById("hub1");
+img1.onclick = function(){
   modal.style.display = "block";
+  var modalImg = document.getElementById("img0");
+  var captionText = document.getElementById("caption");
+}
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+img2.onclick = function(){
+  modal.style.display = "block";
+  var modalImg = document.getElementById("img1");
+  var captionText = document.getElementById("caption");
 }
 
 // Get the <span> element that closes the modal
@@ -24,3 +32,24 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+/*
+https://stackoverflow.com/questions/40572787/how-set-modal-popup-multiple-image
+
+var img2 = document.getElementById('myImg2');
+    var img3 = document.getElementById('myImg3');
+
+    //handle click for myImg2
+    img2.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+
+    //handle click for myImg3
+    img3.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+*/
